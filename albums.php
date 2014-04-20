0276047ae89a69697d82dd6cf8ea2df9c7f86785
imjayson <imjayson@hotmail.com>
2014-04-20 23:20:48 +0800

    removed albums with no cover images (untitled albums) & fixed duplicate albums

--- albums.php
+++ albums.php
@@ -49,7 +49,6 @@
 					// is there a way to get the url by just using standard url 
 					// structure? like just plug it in http://facebook.com/photo/".id."/
 					function getAlbums(data, i)	{
-
 					//skip if album is excluded
 					arrayIndex = jQuery.inArray( data.data[i].id  , excludeAlbums );
 
@@ -89,97 +88,99 @@
 								    	//skipe this album as it is of type cover, wall and profile
 								    	i++;
 								    	getAlbums(data, i );
-								    }
+								    } else {
 
-								    try{	
-  										 								    				    										
-										var imgsrc =  coverPhotoData.picture;
+									    try{	
+									    	if (coverPhotoData.picture) {
+												var imgsrc =  coverPhotoData.picture;
 
-										var albumname = data.data[i].name;
+												var albumname = data.data[i].name;
 
-										var photoslink = "";
-								        //start the row if the item count less than 1
-								        if (rowItemscnt == 1)
-										{
-											curhtml += "<div class=\"row \">";
-											
-										}
-										
-										if(prettypermalinkon==1){
-											photoslink = document.URL+"?fbasid="+data.data[i].id;
-										}else{
-											photoslink = document.URL+"&fbasid="+data.data[i].id;
-											
-										}
+												var photoslink = "";
+										        //start the row if the item count less than 1
+										        if (rowItemscnt == 1)
+												{
+													curhtml += "<div class=\"row \">";
+													
+												}
+												
+												if(prettypermalinkon==1){
+													photoslink = document.URL+"?fbasid="+data.data[i].id;
+												}else{
+													photoslink = document.URL+"&fbasid="+data.data[i].id;
+													
+												}
 
-										//print output 
-										if(rowItemscnt==4){
-													curhtml +="<div class=\"threecol "+rowItemscnt +" last\"><a class=\"albumlink\" href=\""+photoslink+ "\" ><img class=\"albumthumb\" src=\""+ imgsrc +" \" /></a><br/><a class=\"albumlinktitle\"  href=\""+photoslink+ "\">"+ albumname +"</a></div> <!-- last col -->";
-													curhtml += " </div> <!-- End Row Loop-->" ;
-													// output row
-													addhtml(curhtml);
-													curhtml ="";
-													rowItemscnt = 0;
-										}else{
-													curhtml +="<div class=\"threecol "+rowItemscnt +" \"><a class=\"albumlink\"  href=\""+photoslink+ "\" ><img class=\"albumthumb\" src=\""+ imgsrc +" \" /></a><br/><a class=\"albumlinktitle\"  href=\""+photoslink+ "\">"+ albumname +"</a></div>";
-										}
-										
-										// print out current image block
-										//addhtml(curhtml);
-										
-										//increment counters										
-										rowItemscnt ++; 
-										//End ALbum Image processing
-										
-										//check if the if there are more albums left
-										if (i < data.data.length - 1){
-											i++;
-											//console.log("<< Get Next Album>>");
-											getAlbums(data, i );
-										}else
-										{
-											//check if there is another page
-											try
-											{
-										
-
-												  //get the next page
-													if( !jQuery.isEmptyObject(data.paging.next))
-													{
-														getAlbumPage(data.paging.next);
-														
-													}else{
-														jQuery('#fbloader').remove();
-													}
-													jQuery('#fbloader').remove();
-											  }
-											catch(err)
-											  {
-											  	if (rowItemscnt<4){ 
-													// close the row if there are less than four items at the end of all albums
-															curhtml += "</div> <!-- End Row try Catch-->";
+												//print output 
+												if(rowItemscnt==4){
+															curhtml +="<div class=\"threecol "+rowItemscnt +" last\"><a class=\"albumlink\" href=\""+photoslink+ "\" ><img class=\"albumthumb\" src=\""+ imgsrc +" \" /></a><br/><a class=\"albumlinktitle\"  href=\""+photoslink+ "\">"+ albumname +"</a></div> <!-- last col -->";
+															curhtml += " </div> <!-- End Row Loop-->" ;
+															// output row
 															addhtml(curhtml);
-													 		curhtml ="";
-															rowItemscnt = 1;
-												}	
-											  		//Handle errors here
-											  		//console.log("child no more catch: "+err);
-													addhtml("<h4>No More Pages</h4>");
-													jQuery('#fbloader').remove();
-											  }
-	
-										}
-								    }catch(err){ // End of 25 Albums catch
-								    		i++;
-								    		console.log(err);
-								    		if(	!( i in obj) ){
-								    			jQuery('#fbloader').remove();
-								    		} else{
-								    			// get the next batch of albums
-								    			getAlbums(data, i );
-								    		}
+															curhtml ="";
+															rowItemscnt = 0;
+												}else{
+															curhtml +="<div class=\"threecol "+rowItemscnt +" \"><a class=\"albumlink\"  href=\""+photoslink+ "\" ><img class=\"albumthumb\" src=\""+ imgsrc +" \" /></a><br/><a class=\"albumlinktitle\"  href=\""+photoslink+ "\">"+ albumname +"</a></div>";
+												}
+												
+												// print out current image block
+												//addhtml(curhtml);
+												
+												//increment counters										
+												rowItemscnt ++; 
+												//End ALbum Image processing
+											}
+											//check if the if there are more albums left
+											if (i < data.data.length - 1){
+												i++;
+												//console.log("<< Get Next Album>>");
+												getAlbums(data, i );
+											}else
+											{
+												//check if there is another page
+												try
+												{
 											
-								    }								    
+
+													  //get the next page
+														if( !jQuery.isEmptyObject(data.paging.next))
+														{
+															getAlbumPage(data.paging.next);
+															
+														}else{
+															jQuery('#fbloader').remove();
+														}
+														jQuery('#fbloader').remove();
+												  }
+												catch(err)
+												  {
+												  	if (rowItemscnt<4){ 
+														// close the row if there are less than four items at the end of all albums
+																curhtml += "</div> <!-- End Row try Catch-->";
+																addhtml(curhtml);
+														 		curhtml ="";
+																rowItemscnt = 1;
+													}	
+												  		//Handle errors here
+												  		//console.log("child no more catch: "+err);
+														addhtml("<h4>No More Pages</h4>");
+														jQuery('#fbloader').remove();
+												  }
+		
+											}
+										
+									    }catch(err){ // End of 25 Albums catch
+									    		i++;
+									    		console.log(err);
+									    		if(	!( i in obj) ){
+									    			jQuery('#fbloader').remove();
+									    		} else{
+									    			// get the next batch of albums
+									    			getAlbums(data, i );
+									    		}
+												
+									    }
+									}								    
 
 							
 									},
